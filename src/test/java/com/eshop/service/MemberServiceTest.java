9c53d33790e0b49c07fd3385a5bf7207d8532624
sunjung Oh <sspure1214@naver.com>
2024-02-14 14:47:13 +0900

    .

--- src/test/java/com/eshop/service/MemberServiceTest.java
+++ src/test/java/com/eshop/service/MemberServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -48,13 +49,19 @@ class MemberServiceTest {
         assertEquals(member.getRole(), savedMember.getRole());
     }
 
+    @Test
+    @DisplayName("중복 회원 가입 테스트")
+    public void saveDuplicateMemberTest(){
+        Member member1 = createMember();
+        Member member2 = createMember();
 
-//
-//    @Test
-//    @DisplayName("중복 회원 가입 테스트")
-//    public void saveDuplicateMemberTest(){
-//        Member member1 = createMember();
-//
-//
-//    }
+        memberService.saveMember(member1);
+
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            memberService.saveMember(member2);
+        });
+
+        assertEquals("이미 가입된 회원입니다.", e.getMessage());
+
+    }
 }
