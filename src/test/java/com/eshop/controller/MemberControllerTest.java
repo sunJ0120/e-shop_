2bf96b914fa088f270b23b4d2de1343304c94f36
sunjung Oh <sspure1214@naver.com>
2024-02-15 14:54:38 +0900

    login 페이지 구현하기
    1. login 성공, 실패 테스트 통과
    2. 로그아웃, 로그인 상태 연동을 위해 thymleaf-security 연동
    3. header에 관련 내용 추가
    
    * localhost에 접근 -> 회원가입, 로그인 후 로그아웃 버튼으로 바뀌는 것까지 확인 완료

---
+++ src/test/java/com/eshop/controller/MemberControllerTest.java
@@ -0,0 +1,71 @@
+package com.eshop.controller;
+
+import com.eshop.dto.MemberFormDto;
+import com.eshop.entity.Member;
+import com.eshop.service.MemberService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class MemberControllerTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(String email, String password) {
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail(email);
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 은평구 구산동");
+        memberFormDto.setPassword(password);
+
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        return memberService.saveMember(member);
+    }
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    public void loginSuccessTest() throws Exception {
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email, password);
+
+        mockMvc.perform(formLogin().userParameter("email")
+                        .loginProcessingUrl("/members/login")
+                        .user(email).password(password))
+                .andExpect(authenticated());
+    }
+
+    @Test
+    @DisplayName("로그인 실패 테스트")
+    public void loginFaiTest() throws Exception{
+        String email = "test@gmail.com";
+        String password = "1234";
+        this.createMember(email, password);
+        mockMvc.perform(formLogin().userParameter("email")
+                .loginProcessingUrl("/members/login")
+                .user(email).password("12345"))
+                .andExpect(unauthenticated());
+    }
+}
