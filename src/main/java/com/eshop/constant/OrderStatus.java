48c4c82d61555fdb4b7eb838e541bc6aa6a87bc9
sunjung Oh <sspure1214@naver.com>
2024-02-16 14:17:58 +0900

    다대일/일대다 양방향 매핑하기
    - OrderItem, Order, Item 매핑관계 설정 완료
    
    1. OrderItem과 Order는 주문 안에 다양한 아이템이 있으므로 다대일관계
    2. OrderItem과 Item은 하나의 아이템이 다양한 orderItem에 존재할 수 있으므로 다대일관계
    
    * 양방향 매핑을 설정할때는 외래키를 가진(다쪽)쪽에서 연관관계를 관리한다.
    * 가지지 않은 쪽은 mappedBy를 통해 매핑 된 곳을 연결하고, 읽기만 가능하도록 한다.

---
+++ src/main/java/com/eshop/constant/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.eshop.constant;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
---
+++ src/main/java/com/eshop/entity/OrderItem.java
@@ -0,0 +1,30 @@
+package com.eshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice; //주문 가격
+    private int count; //수량
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+}
